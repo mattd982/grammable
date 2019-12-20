1f75e3020a5882c72e5ea0d3673ca8beb85bff6e
matt day <mattdxc982@gmail.com>
2019-12-20 03:07:09 +0000

    lesson 14

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
-class ApplicationController < ActionController::Base
-end
+class GramsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def destroy
+    @gram = Gram.find_by_id(params[:id]
+    return render_not_found if @gram.blank?
+    @gram.destroy
+    redirect_to root_path
+  end
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
-     describe "grams#update action" do
-    it "should allow users to successfully update grams" do
-      gram = FactoryBot.create(:gram, message: "Initial Value")
-      patch :update, params: { id: gram.id, gram: { message: 'Changed' } }
+    describe "grams#destroy action" do
+    it "should allow a user to destroy grams" do
+      gram = FactoryBot.create(:gram
+      delete :destroy, params: { id: gram.id }
       expect(response).to redirect_to root_path
-      gram.reload
-      expect(gram.message).to eq "Changed"
+      gram = Gram.find_by_id(gram.id)
+      expect(gram).to eq nil
+
     end
 
-    it "should have http 404 error if the gram cannot be found" do
-      patch :update, params: { id: "YOLOSWAG", gram: { message: 'Changed' } }
-      expect(response).to have_http_status(:not_found)
+    it "should return a 404 message if we cannot find a gram with the id that is specified" do
+      delete :destroy, params: { id: 'SPACEDUCK'
+      expect(response).to have_http_status(:not_found) 
     end
+  end
 
     it "should render the edit form with an http status of unprocessable_entity" do
       gram = FactoryBot.create(:gram, message: "Initial Value")
