f13bc4e39419075218938e90cdfc3dd58921a708
matt day <mattdxc982@gmail.com>
2019-12-20 02:42:59 +0000

    lesson 12. Same as lesson 11 commit

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,14 @@
 FactoryBot.define do
   factory :user do
     sequence :email do |n|
-      { "dummyEmail#{n}@gmail.com" }
+       { "codingmd982@gmail.com" }
     end
     password { "secretPassword" }
     password_confirmation { "secretPassword" }
+  end
+
+  factory :gram do
+    message { "hello" }
+    association :user
   end
 end
