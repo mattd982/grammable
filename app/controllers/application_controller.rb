843d9d8dc437e85bf5da0540d142af4a28c2cd82
matt day <mattdxc982@gmail.com>
2019-12-20 03:54:57 +0000

    lesson 15, still showing 4 errors in env, but webpage runs fine.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
-class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-
-  def destroy
-    @gram = Gram.find_by_id(params[:id]
-    return render_not_found if @gram.blank?
-    @gram.destroy
-    redirect_to root_path
-  end
+class ApplicationController < ActionController::Base
+end
