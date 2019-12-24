ef86670aa2e1120dcbbd1e6a57f44d96ac90e91b
matt day <mattdxc982@gmail.com>
2019-12-24 04:58:01 +0000

    finished, come back to touch up

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,16 +1,16 @@
-# FactoryBot.define do
-#   factory :user do
-#     sequence :email do |n|
-#       { "dummyEmail#{n}@gmail.com" }
-#     end
-#     password { "secretPassword" }
-#     password_confirmation { "secretPassword" }
-#   end
+FactoryBot.define do
+  factory :user do
+    sequence :email do |n|
+       "dummyEmail#{n}@gmail.com" 
+    end
+    password { "secretPassword" }
+    password_confirmation { "secretPassword" }
+  end
 
-#   factory :gram do
-#     message { "hello" }
-#     picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png').to_s, 'image/png') }
+  factory :gram do
+    message { "hello" }
+    picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png').to_s, 'image/png') }
     
-#     association :user
-#   end
-# end
+    association :user
+  end
+end
